b486fcb3e5b2524cfb595b82a836da2aeb795461
Bill <johnn.condori@tecsup.edu.pe>
2018-03-12 15:33:01 -0500

    bucle en node

--- Lab 1/bucle.js
+++ Lab 1/bucle.js
@@ -1,8 +1,11 @@
 var http = require('http');
 
 var manejador = function(solicitud,respuesta){
-	console.log('Conexion entrante');
-	respuesta.end('Hola mundo!');
+	var i = 0;
+	while(true){
+		respuesta.write(i+'-->')
+		i++
+	}
 };
 
 var servidor = http.createServer(manejador);
