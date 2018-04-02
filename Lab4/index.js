373d875a332d64aeeddf77786f8b4456e6e1b37d
Bill <johnn.condori@tecsup.edu.pe>
2018-04-02 16:07:29 -0500

    Lab4

---
+++ Lab4/index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+	res.send('Hola Bill! en Express 🙂')
+})
+
+app.post('/', function(req, res) {
+	res.send('Llamada POST misma url')
+})
+
+app.put('/user', function(req, res) {
+	res.send('Recibimos un PUT en /user')
+})
+
+app.delete('/user', function(req, res) {
+	res.send('Recibimos un DELETE en /user')
+})
+
+app.listen(3000, function() {
+	console.log('Aplicacion de ejemplo escuchando en el puerto 3000!')
+});
